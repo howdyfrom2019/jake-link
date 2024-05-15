5ccdc54917f97302b7720cf43c2e00076d812907
Jake <tjd1emd@naver.com>
2024-05-15 23:31:28 +0900

    Fix env types

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,9 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_ENVIRONMENT: 'dev' | 'prod';
-    DB_HOST: string;
-    DB_USER: string;
-    DB_PASSWORD: string;
-    DB_DATABASE: string;
+    DATABASE_URL: string;
   }
 }
