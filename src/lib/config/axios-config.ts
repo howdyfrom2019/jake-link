73994298b5777cac6e1cd8b81ae164baf992a948
howdyfrom2019 <tjd1emd@naver.com>
2024-05-15 17:47:30 +0900

    Fix host with env

--- src/lib/config/axios-config.ts
+++ src/lib/config/axios-config.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const jakeLinkService = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL:
+    process.env.NEXT_PUBLIC_ENVIRONMENT === 'dev'
+      ? 'http://localhost:3000'
+      : '',
   headers: {
     'Content-Type': 'application/json',
   },
